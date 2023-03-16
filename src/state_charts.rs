8a739f2270a9883f3ed02812e033812ce7fe0f3e
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2023-03-16 19:36:21 +0100

    First unit tests.

--- src/state_charts.rs
+++ src/state_charts.rs
@@ -2,6 +2,21 @@ use std::collections::BTreeMap;
 use open_api_matcher::ValidatedValue;
 use crate::error::StateChartError;
 
+/// A system wide unique Id for a node.
+type NodeId = String;
+
+/// A system wide unique Id of a action.
+type ActionId = String;
+
+/// A system wide unique Id of a variable.
+type VariableId = String;
+
+/// A system wide unique Id of a event.
+type EventId = String;
+
+/// A system wide unique Id of a predicate.
+type PredicateId = String;
+
 /// The node is the heard of the state chart definition. A node can be a single state or a state
 /// chart of its own.
 pub struct Node {
@@ -14,62 +29,35 @@ pub struct Node {
     out_transitions: Vec<Transition>,
     attributes: Vec<VariableDeclarations>,
 }
-impl Node {
-    pub fn from_validate_value(object: &ValidatedValue) -> Result<Self, StateChartError> {
-        if let ValidatedValue::Object(attributes) = object {
+/// Constructs a node from the validated value.
+impl TryFrom<&ValidatedValue> for Node {
+    type Error = StateChartError;
 
-            // let vid = get_mandatory_attribute(attributes, "Id")?;
-            // let id: String = vid.try_into()?;
-            // return Ok(Node::default());
-            let id = get_mandatory(attributes, "id")?;
+    fn try_from(value: &ValidatedValue) -> Result<Self, Self::Error>
+    {
+        if let ValidatedValue::Object(attributes) = value {
+
+            let id = get_mandatory(&attributes, "id")?;
             let id: String = id.try_into()?;
-            let name = get_mandatory(attributes, "name")?;
+            let name = get_mandatory(&attributes, "name")?;
             let name: String = name.try_into()?;
 
             Ok(Node {
                 id,
                 name,
-                description: get_mandatory(attributes, "description")?.try_into()?,
-                on_entry: get_mandatory(attributes, "on_entry")?.try_into()?,
-                on_exit: get_mandatory(attributes, "on_exit")?.try_into()?,
-                start_node: Box::new(Node::from_validate_value(
-                        get_mandatory(attributes, "start_node")?)?),
-                out_transitions: Node::transitions_from_validated_value(get_mandatory(attributes, "out_transitions")?)?,
-                attributes: Node::attributes_from_validated_value(get_mandatory(attributes, "attributes")?)?,
+                description: get_mandatory(&attributes, "description")?.try_into()?,
+                on_entry: get_mandatory(&attributes, "on_entry")?.try_into()?,
+                on_exit: get_mandatory(&attributes, "on_exit")?.try_into()?,
+                start_node: Box::new(get_mandatory(&attributes, "start_node")?.try_into()?),
+                out_transitions: transitions_from_validated_value(get_mandatory(attributes, "out_transitions")?)?,
+                attributes: attributes_from_validated_value(get_mandatory(attributes, "attributes")?)?,
             })
         }
         else {
             Err(StateChartError::UnexpectedType)
         }
     }
-
-    fn transitions_from_validated_value(vtransitions: &ValidatedValue)
-        -> Result<Vec<Transition>, StateChartError>
-    {
-        Ok(Vec::new())
-    }
-
-    fn attributes_from_validated_value(vattributes: &ValidatedValue)
-        -> Result<Vec<VariableDeclarations>, StateChartError>
-    {
-        Ok(Vec::new())
-    }
 }
-
-/// Retrieves a mandatory attribute from a standard map.
-fn get_mandatory<'a>(attributes: &'a BTreeMap<String, ValidatedValue>, name: &str) 
-    -> Result<&'a ValidatedValue, StateChartError>
-{
-    if let Some(attribute) = attributes.get(name) {
-        Ok(attribute)
-    }
-    else {
-        Err(StateChartError::MandatoryAttributeMissing(name.into()))
-    }
-}
-
-/// A system wide unique Id for a node. The for is defined in the OpenApi specification.
-type NodeId = String;
 
 pub struct ActionCall {
     name: ActionId,
@@ -92,14 +80,6 @@ impl TryFrom<&ValidatedValue> for ActionCall {
     }
 }
 
-fn parameters_from_validated_values(values: &ValidatedValue)
-    -> Result<Vec<Parameter>, StateChartError>
-{
-    Ok(Vec::new())
-}
-
-type ActionId = String;
-
 pub struct Parameter {
     name: VariableId,
     value: VariableValue,
@@ -119,8 +99,6 @@ impl TryFrom<&ValidatedValue> for Parameter {
     }
 }
 
-type VariableId = String;
-
 pub struct Transition {
     /// The check, which tests, if the transaction is activated.
     guard: Guard,
@@ -130,20 +108,21 @@ pub struct Transition {
     action: ActionCall,
 }
 
+/// The guard on a trasition holds the condition under which a transaction is activated.
+/// It will be evaluated by the state machine runtime.
 pub enum Guard {
     Event(EventId),
     Predicate(PredicateCall),
 }
 
-type EventId = String;
-
+/// The call of a predicate may be a guard. The predicate of all transactions of the current state
+/// will be evaluated when ever a variable value was modified.
 pub struct PredicateCall {
     name: PredicateId,
     parameters: Vec<Parameter>,
 }
 
-type PredicateId = String;
-
+/// Declares a variable inside of a state chart state.
 #[derive(Default)]
 pub struct VariableDeclarations {
     name: String,
@@ -151,6 +130,7 @@ pub struct VariableDeclarations {
     value: VariableValue,
 }
 
+/// The variable value holds the value of a variable attribute or parameter.
 pub enum VariableValue {
     String(String),
     Integer(i64),
@@ -177,4 +157,88 @@ impl TryFrom<&ValidatedValue> for VariableValue {
             _ => Err(StateChartError::UnexpectedType),
         }
     }
+}
+
+/// Retrieves a mandatory attribute from a standard map.
+fn get_mandatory<'a>(attributes: &'a BTreeMap<String, ValidatedValue>, name: &str) 
+    -> Result<&'a ValidatedValue, StateChartError>
+{
+    if let Some(attribute) = attributes.get(name) {
+        Ok(attribute)
+    }
+    else {
+        Err(StateChartError::MandatoryAttributeMissing(name.into()))
+    }
+}
+
+fn transitions_from_validated_value(vtransitions: &ValidatedValue)
+    -> Result<Vec<Transition>, StateChartError>
+{
+    Ok(Vec::new())
+}
+
+fn attributes_from_validated_value(vattributes: &ValidatedValue)
+    -> Result<Vec<VariableDeclarations>, StateChartError>
+{
+    Ok(Vec::new())
+}
+
+/// Derives a vector of parameters from an array of validated values.
+fn parameters_from_validated_values(values: &ValidatedValue)
+    -> Result<Vec<Parameter>, StateChartError>
+{
+    if let ValidatedValue::Array(values) = values {
+        let mut result = Vec::new();
+        for v_param in values {
+            let param: Parameter = v_param.try_into()?;
+            result.push(param);
+        }
+        Ok(result)
+    }
+    else {
+        Err(StateChartError::UnexpectedType)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test_get_mandatory() {
+        let mut attributes = BTreeMap::new();
+        get_mandatory(&attributes, "name").is_err();
+        attributes.insert("name".into(), ValidatedValue::String("Lucky".into()));
+        get_mandatory(&attributes, "name").unwrap();
+    }
+
+    #[test]
+    fn test_parameter_try_into() {
+
+        let mut parameter_a: BTreeMap<String, ValidatedValue> = BTreeMap::new();
+        parameter_a.insert("name".into(), ValidatedValue::String("A".into()));
+        parameter_a.insert("value".into(), ValidatedValue::Bool(true));
+
+        let parameter: Parameter = (&ValidatedValue::Object(parameter_a)).try_into().unwrap();
+    }
+
+    #[test]
+    fn test_parameters_from_vv() {
+        let mut parameter_a: BTreeMap<String, ValidatedValue> = BTreeMap::new();
+        parameter_a.insert("name".into(), ValidatedValue::String("A".into()));
+        parameter_a.insert("value".into(), ValidatedValue::Bool(true));
+        let mut parameter_b: BTreeMap<String, ValidatedValue> = BTreeMap::new();
+        parameter_a.insert("name".into(), ValidatedValue::String("B".into()));
+        parameter_a.insert("value".into(), ValidatedValue::Bool(false));
+        let mut v_parameters: Vec<ValidatedValue> = Vec::new();
+        v_parameters.push(ValidatedValue::Object(parameter_a));
+        v_parameters.push(ValidatedValue::Object(parameter_b));
+
+        // Check, if the paramters can be extracted as exprected.
+        let parameters = parameters_from_validated_values(&ValidatedValue::Array(v_parameters)).unwrap();
+        assert_eq!(2, parameters.len());
+    }
+
 }
